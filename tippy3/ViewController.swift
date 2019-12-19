86da784228d3181862ea4e2854b03312906a832e
Neri <neri.garciafigueroa@gmail.com>
2019-12-18 19:18:23 -0800

    Initial Commit

---
+++ tippy3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy3
+//
+//  Created by Neri Garcia on 12/18/19.
+//  Copyright © 2019 Neri Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tippy3Tests/tippy3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  tippy3Tests.swift
+//  tippy3Tests
+//
+//  Created by Neri Garcia on 12/18/19.
+//  Copyright © 2019 Neri Garcia. All rights reserved.
+//
+
+import XCTest
+@testable import tippy3
+
+class tippy3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
